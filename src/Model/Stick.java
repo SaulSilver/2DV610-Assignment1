9afd252924a04a4864ce02721a3fd21d32706cce
SaulSilver <hatem.houssein@gmail.com>
2015-11-12 11:14:10 +0100

    REFACTOR

--- src/Model/Stick.java
+++ src/Model/Stick.java
@@ -4,6 +4,11 @@ package Model;
  * Created by Hatem on 11/12/2015.
  */
 public class Stick {
+    char stick;
+
+    public Stick() {
+        stick = 'I';
+    }
 
     public Character getChar() {
         return ' ';
