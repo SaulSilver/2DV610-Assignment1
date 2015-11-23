2bcb4000ec99ba94825814859467d35da11af9c9
SaulSilver <hatem.houssein@gmail.com>
2015-11-23 13:51:15 +0100

    GREEN

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -3,11 +3,17 @@ package Controller;
 import Model.Game;
 import View.Console;
 
+import java.io.IOException;
+
 /**
  * Created by Hatem on 11/23/2015.
  */
 public class GameController {
-    public int StartUpGame(Game game, Console view) {
-        return 0;
+    Console m_console;
+
+    public int StartUpGame(Game game, Console view) throws IOException {
+        m_console = view;
+        char input = m_console.GetStartUpInput();
+        return m_console.CheckInput(input);
     }
 }
