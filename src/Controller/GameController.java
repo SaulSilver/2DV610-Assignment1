1c691a4624d394e800dae5a92b57087c069fca84
SaulSilver <hatem.houssein@gmail.com>
2015-11-23 13:44:10 +0100

    RED

---
+++ src/Controller/GameController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import Model.Game;
+import View.Console;
+
+/**
+ * Created by Hatem on 11/23/2015.
+ */
+public class GameController {
+    public int StartUpGame(Game game, Console view) {
+        return 0;
+    }
+}
---
+++ test/ControllerTest/GameControllerTest.java
@@ -0,0 +1,37 @@
+package ControllerTest;
+
+import Controller.GameController;
+import Model.Game;
+import View.Console;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Hatem on 11/23/2015.
+ */
+public class GameControllerTest {
+
+    GameController controller;
+    Console view;
+    Game  game;
+
+    @Before
+    public void setUp() throws Exception {
+        view = mock(Console.class);
+        game = mock(Game.class);
+        controller = new GameController();
+    }
+
+    @Test
+    public void ShouldShowStartUpMessagesAndReturnUserChoiceAsCharacter() throws IOException {
+        when(view.GetStartUpInput()).thenReturn('p');
+        when(view.CheckInput('p')).thenReturn(1);
+        assertTrue(1 == controller.StartUpGame(game, view));
+
+    }
+}
